81f7256741e7325d80a7cd4f5beb7f80b11bc620
anhduc151 <85023526+anhduc151@users.noreply.github.com>
2023-04-18 10:12:53 +0700

    Update index.js

--- src/TutorPages/Courses/index.js
+++ src/TutorPages/Courses/index.js
@@ -3,10 +3,20 @@ import { Avatar, Dropdown, Menu } from 'antd';
 import { DownOutlined } from '@ant-design/icons';
 import user from '../../assets/images/user.jpg'
 import { Link } from 'react-router-dom'
-
-
 import React from 'react';
 import { Segmented } from 'antd';
+import avt1 from '../../assets/images/avt1.jpg'
+import avt2 from '../../assets/images/avt2.jpg'
+import avt3 from '../../assets/images/avt3.jpg'
+import avt4 from '../../assets/images/avt4.jpg'
+import avt5 from '../../assets/images/avt5.jpg'
+import avt6 from '../../assets/images/avt6.jpg'
+import courses4 from '../../assets/images/courses4.jpg'
+import courses2 from '../../assets/images/courses2.jpg'
+import courses1 from '../../assets/images/courses1.jpg'
+import courses3 from '../../assets/images/courses3.jpg'
+import courses5 from '../../assets/images/courses5.jpg'
+import courses6 from '../../assets/images/courses6.jpg'
 
 
 function myCoursestt() {
@@ -93,11 +103,11 @@ function myCoursestt() {
               <h1>Tutor</h1>
               <ul className="course__breadcrumb">
                 <li>
-                  <a href="#">Dashboard</a>
+                  <a href="">Dashboard</a>
                 </li>
                 <li><i className="bx bx-chevron-right" /></li>
                 <li>
-                  <a href="#">Home</a>
+                  <a href="">Course</a>
                 </li>
               </ul>
             </div>
@@ -105,20 +115,149 @@ function myCoursestt() {
 
           <div className="course-container">
             <div className="title-container">
-              <h1 className='course__h1tittle'>Course</h1>
-              <button className="add-course">Add Course</button>
+              <h1 className='course__h1tittle'>Teaching Course</h1>
+              <Link to="/addcourse"><button className="add-course">Add Course</button></Link>
             </div>
-            <div className="course-list">
-              <div className="course">Course 1</div>
-              <div className="course">Course 2</div>
-              <div className="course">Course 3</div>
-              <div className="course">Course 4</div>
-              <div className="course">Course 5</div>
-              <div className="course">Course 6</div>
-              <div className="course">Course 7</div>
-              <div className="course">Course 8</div>
-              <div className="course">Course 9</div>
+            {/* <div className='course__padding'> */}   
+            <Segmented options={['Technology', 'Languages', 'Economics', 'Marketing', 'Design']} className='course__segmented' />
+            <div className="student__box">
+
+              <div className="box__student">
+                <div className="student__thumb">
+                  <img src={courses1} alt="" className="student__img2" />
+                  <span className="student__spana">10 unit</span>
+                </div>
+                <div className="student__tutor">
+                  <img src={avt1} alt="" className="student__img1" />
+                  <div className="student__info">
+                    <h3 className="student__h32">Rebecca</h3>
+                  </div>
+                </div>
+                <h3 className="student__title">complete HTML tutorial</h3>
+                <p className='student__des'>Good friends, good books, and a sleepy conscience: this is the ideal life.</p>
+                <a href="playlist.html" className="inline-btn">Learn More</a>
+              </div>
+
+              <div className="box__student">
+                <div className="student__thumb">
+                  <img src={courses2} alt="" className="student__img2" />
+                  <span className="student__spana">10 unit</span>
+                </div>
+                <div className="student__tutor">
+                  <img src={avt2} alt="" className="student__img1" />
+                  <div className="student__info">
+                    <h3 className="student__h32">David</h3>
+                  </div>
+                </div>
+                <h3 className="student__title">complete CSS tutorial</h3>
+                <p className='student__des'>then you sure as hell don't deserve me at my best</p>
+                <a href="playlist.html" className="inline-btn">Learn More</a>
+              </div>
+
+              <div className="box__student">
+                <div className="student__thumb">
+                  <img src={courses3} alt="" className="student__img2" />
+                  <span className="student__spana">10 Unit</span>
+                </div>
+                <div className="student__tutor">
+                  <img src={avt3} alt="" className="student__img1" />
+                  <div className="student__info">
+                    <h3 className="student__h32">Christopher</h3>
+                  </div>
+                </div>
+
+                <h3 className="student__title">complete JS tutorial</h3>
+                <p className='student__des'>You've gotta dance like there's nobody watching</p>
+                <a href="playlist.html" className="inline-btn">Learn More</a>
+              </div>
+
+              <div className="box__student">
+                <div className="student__thumb">
+                  <img src={courses4} alt="" className="student__img2" />
+                  <span className="student__spana">10 Unit</span>
+                </div>
+                <div className="student__tutor">
+                  <img src={avt4} alt="" className="student__img1" />
+                  <div className="student__info">
+                    <h3 className="student__h32">John</h3>
+                  </div>
+                </div>
+
+                <h3 className="student__title">complete Boostrap tutorial</h3>
+                <p className='student__des'>You only live once, but if you do it right, once is enough.</p>
+                <a href="playlist.html" className="inline-btn">Learn More</a>
+              </div>
+
+              <div className="box__student">
+                <div className="student__thumb">
+                  <img src={courses5} alt="" className="student__img2" />
+                  <span className="student__spana">10 Unit</span>
+                </div>
+                <div className="student__tutor">
+                  <img src={avt5} alt="" className="student__img1" />
+                  <div className="student__info">
+                    <h3 className="student__h32">Kevin</h3>
+                  </div>
+                </div>
+
+                <h3 className="student__title">complete JQuery tutorial</h3>
+                <p className='student__des'>In three words I can sum up everything I’ve learned about life</p>
+                <a href="playlist.html" className="inline-btn">Learn More</a>
+              </div>
+
+              <div className="box__student">
+
+                <div className="student__thumb">
+                  <img src={courses6} alt="" className="student__img2" />
+                  <span className="student__spana">10 Unit</span>
+                </div>
+                <div className="student__tutor">
+                  <img src={avt6} alt="" className="student__img1" />
+                  <div className="student__info">
+                    <h3 className="student__h32">Mark</h3>
+                  </div>
+                </div>
+                <h3 className="student__title">complete SASS tutorial</h3>
+                <p className='student__des'>To live is the rarest thing in the world. Most people exist, that is all. </p>
+                <a href="playlist.html" className="inline-btn">Learn More</a>
+              </div>
+
+              <div className="box__student">
+
+                  <div className="student__thumb">
+                    <img src={courses6} alt="" className="student__img2" />
+                    <span className="student__spana">10 Unit</span>
+                  </div>
+                  <div className="student__tutor">
+                    <img src={avt6} alt="" className="student__img1" />
+                    <div className="student__info">
+                      <h3 className="student__h32">Mark</h3>
+                    </div>
+                  </div>
+                  <h3 className="student__title">complete SASS tutorial</h3>
+                  <p className='student__des'>To live is the rarest thing in the world. Most people exist, that is all. </p>
+                  <a href="playlist.html" className="inline-btn">Learn More</a>
+                  </div>
+
+                  <div className="box__student">
+
+                  <div className="student__thumb">
+                    <img src={courses6} alt="" className="student__img2" />
+                    <span className="student__spana">10 Unit</span>
+                  </div>
+                  <div className="student__tutor">
+                    <img src={avt6} alt="" className="student__img1" />
+                    <div className="student__info">
+                      <h3 className="student__h32">Mark</h3>
+                    </div>
+                  </div>
+                  <h3 className="student__title">complete SASS tutorial</h3>
+                  <p className='student__des'>To live is the rarest thing in the world. Most people exist, that is all. </p>
+                  <a href="playlist.html" className="inline-btn">Learn More</a>
+                  </div>
+
             </div>
+            {/* </div> */}
           </div>
 
           
